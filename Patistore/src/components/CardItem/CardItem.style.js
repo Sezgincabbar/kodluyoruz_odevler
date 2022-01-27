7094c037288d34553d1965950604bc528f8ce756
Abdulcabbar Sezgin <sezgin.cabbar01@gmail.com>
2022-01-27 14:42:02 +0300

    patikastore

--- Patistore/src/components/CardItem/CardItem.style.js
+++ Patistore/src/components/CardItem/CardItem.style.js
@@ -4,26 +4,30 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#eceff1',
-    padding: 15,
-    margin: 10,
-    marginRight: 10,
+    padding: 5,
+    margin: 5,
     borderRadius: 10,
   },
   card_image: {
     height: Dimensions.get('window').height / 4,
     width: '100%',
     borderRadius: 10,
+    resizeMode: 'contain',
   },
   card_title: {
-    fontSize: 25,
+    fontSize: 20,
     color: 'black',
+    marginBottom: 15,
+    fontWeight: 'bold',
   },
   card_price: {
     fontSize: 18,
+    fontWeight: '700',
   },
   card_status: {
     color: '#FF0000',
     fontSize: 18,
     textTransform: 'uppercase',
+    fontWeight: '700',
   },
 });
